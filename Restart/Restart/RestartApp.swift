ae10921cebf06e2e8f610c6cd87524dd5624164f
Erkan Öztürk <erkanozturk1903@yahoo.com.tr>
2023-10-22 14:45:22 +0300

    V2

---
+++ Restart/Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Erkan Ozturk on 19.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Restart/Restart/Screens/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  Restart
+//
+//  Created by Erkan Ozturk on 19.10.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @AppStorage("onboarding") var isOnboardingViewActive: Bool = true
+    var body: some View {
+        ZStack {
+           if isOnboardingViewActive {
+                OnboardingView()
+            }else {
+                HomeView()
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
