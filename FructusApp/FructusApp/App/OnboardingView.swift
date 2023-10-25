45aeb5e7a0001095fef24f91376c89dbd2e1b01d
Erkan Öztürk <erkanozturk1903@yahoo.com.tr>
2023-10-26 02:04:31 +0300

    V5

---
+++ FructusApp/FructusApp/App/OnboardingView.swift
@@ -0,0 +1,35 @@
+//
+//  OnboardingView.swift
+//  FructusApp
+//
+//  Created by Erkan Ozturk on 26.10.2023.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    // MARK: - PROPERTIES
+
+    var fruits: [Fruit] = fruitsData
+
+    // MARK: - BODY
+    var body: some View {
+        TabView{
+            ForEach(fruits[0...5]) {item in
+                FruitCardView(fruit: item)
+            }//: LOOP
+
+        }//: TAB
+        .tabViewStyle(PageTabViewStyle())
+        .padding(.vertical, 20)
+    }
+}
+
+// MARK: - PREVIEW
+
+struct OnboardingView_Previews: PreviewProvider {
+    static var previews: some View {
+        OnboardingView(fruits: fruitsData)
+            .previewDevice("iPhone 14 Pro")
+    }
+}
---
+++ FructusApp/FructusApp/Model/FruitModel.swift
@@ -0,0 +1,20 @@
+//
+//  FruitModel.swift
+//  FructusApp
+//
+//  Created by Erkan Ozturk on 26.10.2023.
+//
+
+import SwiftUI
+
+//MARK: - FRUIT DATA MODEL
+
+struct Fruit: Identifiable {
+    var id = UUID()
+    var title: String
+    var headline: String
+    var image: String
+    var gradientColors: [Color]
+    var description: String
+    var nutrition: [String]
+}
