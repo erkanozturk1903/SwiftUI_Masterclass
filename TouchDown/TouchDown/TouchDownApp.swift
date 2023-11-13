f9900b305034aa4f084ad734403852048472a413
Erkan Öztürk <erkanozturk1903@yahoo.com.tr>
2023-11-13 20:52:12 +0300

    V0

---
+++ TouchDown/TouchDown/TouchDownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchDownApp.swift
+//  TouchDown
+//
+//  Created by Erkan Ozturk on 13.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct TouchDownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
